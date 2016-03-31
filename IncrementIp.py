ac239f1ce2aef9efe662df4f493b26609f829644
Gajendra D Ambi <gajuambi@gmail.com>
2016-04-01 00:42:58 +0530

    Update IncrementIp.py

--- IncrementIp.py
+++ IncrementIp.py
@@ -13,19 +13,19 @@
     github.com/gajuambi/python
 '''
 #get the initial ip address from the user
-a = input("Initial Ip Address: ")
+IPADDRESS = input("Initial Ip Address: ")
 #import the ipaddress module and also check whether it is an ipv6 or ipv4
 import ipaddress
-if ':' in a:
-    b = ipaddress.IPv6Address(a)
+if ':' in IPADDRESS:
+    IPADDRESSMOD = ipaddress.IPv6Address(IPADDRESS)
     print ('this is ipv6 address')
 else:
-    b = ipaddress.IPv4Address(a)
+    IPADDRESSMOD = ipaddress.IPv4Address(IPADDRESS)
     print ('this is ipv4')
 #get the user input on how many ip address to be generated
-c = input("Number of Ip Addresses to be generated: ")
-c = int(c)
-d = b+c
-while b < d:
-    b += 1
-    print(b)
+IPADDRESSES = input("Number of Ip Addresses to be generated: ")
+IPADDRESSES = int(c)
+IPADDRESSES = IPADDRESSMOD+IPADDRESSES
+while IPADDRESSMOD < IPADDRESSES:
+    IPADDRESSMOD += 1
+    print(IPADDRESSMOD)
